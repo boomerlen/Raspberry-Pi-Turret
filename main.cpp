ce631abf6cd74076d18df0e12e10eb283c783683
Hugo Sebesta <bunnieweirdo@gmail.com>
2018-10-28 11:03:08 +1100

    Fisrt Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define GPIO_GUN_MOTOR_SIGNAL_PIN 4 // GPIO4 or pin 7
+#define GPIO_BASE_MOTOR_SIGNAL_PIN 17 // GPIO 17 or pin 11
+#define GPIO_TIP_MOTOR_SIGNAL_PIN 27 // GPIO 27 or pin 13
+#define GPIO_REAR_MOTOR_SIGNAL_PIN 22 // GPIO 22 or pin 15
+#define GPIO_GROUND 14 // GND
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}

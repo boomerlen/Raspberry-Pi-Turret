4c8e51a8b63e08b608f34be5f0f4a58465b0081d
boomerlen <boomerlen@users.noreply.github.com>
2018-10-28 12:53:10 +1100

    Update main.cpp
    Added some defines but no functionality

--- main.cpp
+++ main.cpp
@@ -6,7 +6,22 @@
 #define GPIO_REAR_MOTOR_SIGNAL_PIN 22 // GPIO 22 or pin 15
 #define GPIO_GROUND 14 // GND
 
+#define COMMAND_FIRE 1
+#define COMMAND_UP 2
+#define COMMAND_DOWN 3
+#define COMMAND_RIGHT 4
+#deinfe COMMAND_LEFT 5
+
+#define RETURN_GOOD 0
+#define RETURN_BAD 1
+
 using namespace std;
+
+// Code has to:
+// Do whatever setup is necessary to output to GPIO
+// Contact webserver to get commands
+// Download commands and evaluate them
+// Also keep track of servo positions so that when the height motors max out we can return some other code at them
 
 int main()
 {
